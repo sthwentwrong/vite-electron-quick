67b45589faa6977ca6098ca3d8b7827517ef9d9e
Shiming Ge <P0010901@DESKTOP-SHV9KNU.WX.MSOF.COM>
2020-12-07 11:04:51 +0800

    add icon in table column

--- src/render/views/Index.vue
+++ src/render/views/Index.vue
@@ -1,8 +1,36 @@
 <template>
-  <el-table :data="tableData">
-    <el-table-column prop="date" label="日期" width="140"> </el-table-column>
-    <el-table-column prop="name" label="姓名" width="120"> </el-table-column>
-    <el-table-column prop="address" label="地址"> </el-table-column>
+  <el-table
+    :data="tableData"
+    style="width: 100%; max-height=250"
+    :stripe="true"
+  >
+    <el-table-column prop="date" fixed label="日期" width="140">
+      <template #default="scope">
+        <i class="el-icon-time"></i>
+        <span style="margin-left: 10px">{{ scope.row.date }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column prop="name" label="姓名" width="100"> </el-table-column>
+    <el-table-column prop="address" label="地址">
+      <template #default="scope">
+        <i class="el-icon-location-information" />
+        <span style="margin-left: 10px">{{ scope.row.address }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="操作">
+      <template #default="scope">
+        <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">
+          <i class="el-icon-edit" /> 编辑
+        </el-button>
+        <el-button
+          size="mini"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)"
+        >
+          <i class="el-icon-document-delete" /> 删除
+        </el-button>
+      </template>
+    </el-table-column>
   </el-table>
 </template>
 
@@ -11,13 +39,21 @@ export default {
   name: "Index",
   data() {
     const item = {
-      date: "2016-05-02",
-      name: "王小虎",
-      address: "上海市普陀区金沙江路 1518 弄",
+      date: "2020-12-07",
+      name: "舒舞影",
+      address: "无锡市滨湖区xx小区xx-1703",
     };
     return {
       tableData: Array(20).fill(item),
     };
+  },
+  methods: {
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    handleDelete(index, row) {
+      console.log(index, row);
+    },
   },
 };
 </script>
